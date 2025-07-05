8cc76e855b6ea726ec8f076afb2a5b78b34d02e6
kgwny <210300265+kgwny@users.noreply.github.com>
2025-07-06 00:25:08 +0900

    update main.go

--- main.go
+++ main.go
@@ -1,22 +1,39 @@
 package main
 
 import (
-	"my-app/controller"
 	"my-app/model"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
-func main() {
-	e := echo.New()
+func connect(c echo.Context) error {
+
 	db, _ := model.DB.DB()
-
 	defer db.Close()
+	err := db.Ping()
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "DB接続に失敗しました。")
+	} else {
+		return c.String(http.StatusOK, "DB接続に成功しました。")
+	}
+}
 
-	e.GET("/users", controller.GetUsers)
-	e.GET("/users/:id", controller.GetUser)
-	e.POST("/users", controller.CreateUser)
-	e.PUT("/users/:id", controller.UpdateUser)
-	e.DELETE("/users/id,", controller.DeleteUser)
+func main() {
+
+	e := echo.New()
+	e.GET("/", connect)
 	e.Logger.Fatal(e.Start(":8080"))
+
+	// e := echo.New()
+	// db, _ := model.DB.DB()
+
+	// defer db.Close()
+
+	// e.GET("/users", controller.GetUsers)
+	// e.GET("/users/:id", controller.GetUser)
+	// e.POST("/users", controller.CreateUser)
+	// e.PUT("/users/:id", controller.UpdateUser)
+	// e.DELETE("/users/id,", controller.DeleteUser)
+	// e.Logger.Fatal(e.Start(":8080"))
 }
