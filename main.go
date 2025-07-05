f13185ae39448c31d03f4e11f75cbd34f281c7df
kgwny <210300265+kgwny@users.noreply.github.com>
2025-07-05 23:05:16 +0900

    add main, controller, model

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"my-app/controller"
+	"my-app/model"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	db, _ := model.DB.DB()
+
+	defer db.Close()
+
+	e.GET("/users", controller.GetUsers)
+	e.GET("/users/:id", controller.GetUser)
+	e.POST("/users", controller.CreateUser)
+	e.PUT("/users/:id", controller.UpdateUser)
+	e.DELETE("/users/id,", controller.DeleteUser)
+	e.Logger.Fatal(e.Start(":8080"))
+}
