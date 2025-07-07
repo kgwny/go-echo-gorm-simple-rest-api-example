8368b7650bb6e4bc1b89f351a8bf7cba7eac0f6f
kgwny <210300265+kgwny@users.noreply.github.com>
2025-07-07 23:54:31 +0900

    update db.go

--- model/db.go
+++ model/db.go
@@ -14,7 +14,7 @@ func init() {
 	dsn := "tester:password@tcp(db:3306)/test?charset=utf8mb4&parseTime=True&loc=Local"
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatalln(dsn + "database can't connect")
+		log.Fatalln(dsn + "\ndatabase can't connect.")
 	}
 	DB.AutoMigrate(&User{})
 }
